73c29386ae979670577a3ed67426e34eb7f9de98
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-05-29 23:18:37 -0700

    Add dev key to config options.

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const Generate = require('yeoman-generator');
 module.exports = class WebpackGenerator extends Generator {
   constructor(args, opts) {
     super(args, opts);
-    // Initialize generator here
+    // Each key generates a webpack config file:
+    // dev => development webpack config
+    opts.env.configurations = {
+      dev: {
+        topScope: [
+          // Fn definitions & imports
+        ],
+        webpackOptions: {
+          // Webpack config
+        }
+      }
+    }
   }
   // Prompt user preferences
   prompting() {}
