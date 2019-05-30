26710b51d1844bf3e7182c974de5d6973608917b
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-05-29 21:55:59 -0700

    Being scaffold creation:  - Initialize yarn  - Install yeoman-generator package  - Add index.js  - Create generator class

---
+++ index.js
@@ -0,0 +1,14 @@
+const Generate = require('yeoman-generator');
+
+module.exports = class WebpackGenerator extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+    // Initialize generator here
+  }
+  // Prompt user preferences
+  prompting() {}
+  // Write config files to system
+  writing() {}
+  // Install dependencies
+  install() {}
+};
