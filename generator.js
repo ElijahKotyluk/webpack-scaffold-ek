ad21767abe1834923826320c54c4f7ef634a3f8d
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-05-30 20:23:51 -0700

    Add ternarys for prompt answers.

--- generator.js
+++ generator.js
@@ -1,6 +1,6 @@
-const Generate = require('yeoman-generator');
+const Generator = require('yeoman-generator');
 // import webpack-scaffold helper functions:
-const { List, Input } = require('@webpack-cli/webpack-scaffold');
+const { List, Input, InputValidate } = require('@webpack-cli/webpack-scaffold');
 
 module.exports = class WebpackGenerator extends Generator {
   constructor(args, opts) {
@@ -15,16 +15,40 @@ module.exports = class WebpackGenerator extends Generator {
         webpackOptions: {
           // Webpack config
         }
-      }
+      },
+      // Production configuration:
+      prod: {}
     }
   }
   // Prompt user preferences
   prompting() {
-    return this.prompt({
-      // Prompt questions for user:
-      Input('project-name', 'What would you like to name your project? (my-project)')
-    }).then(answers => {
+
+    // Validate project name:
+    const validateName = (value) => {
+      // Check if project name contains spaces:
+      if(value.indexOf(' ') > 0) {
+        return 'Invalid name: spaces are not allowed, please try again.';
+      } else {
+        return true
+      }
+    }
+    // Prompt questions for user:
+    return this.prompt([
+      InputValidate('name', 'How do you want to name your project? (my-vue-project)', validateName),
+      Input('srcFolder', 'Which folder will your source code be in? (src)'),
+      Input('entry', 'Which is the entry point of your app? (main.js)'),
+      Input('outputFolder', 'Which folder will your generated bundles be in? (dist)'),
+      Input('publicFolder', 'Which folder will your public assets be in? (public)'),
+      // => List, lets the user select it's preferred choice from a list (e.g. yarn, npm)
+      List('manager', 'Which package manager do you prefer?', ['yarn', 'npm'])
+    ]).then(answers => {
       // Do something with user's answers:
+      this.answers = answers;
+      this.answers.name = (answers.name === '') ? answers.name.toLowerCase() : 'my-vue-project';
+      this.answers.srcFolder = (answers.srcFolder === '') ? answers.srcFolder : 'src';
+      this.answers.entry = (answers.entry === '') ? answers.entry : 'main';
+      this.answers.outputFolder = (answers.outputFolder === '') ? answers.outputFolder : 'dist';
+      this.answers.publicFolder = (answers.publicFolder === '') ? answers.publicFolder : 'public';
     });
   }
   // Write config files to system
