da0a55ddc12fb939876503df7ea799342a754203
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-01 22:08:33 -0700

    Add scaffold templates.

---
+++ templates/src/js/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ this.greet }}</h1>
+    <hello-world :greet="greet" />
+  </div>
+</template>
+
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+  data: () => ({
+    greet: 'it worked'
+  })
+}
+</script>
