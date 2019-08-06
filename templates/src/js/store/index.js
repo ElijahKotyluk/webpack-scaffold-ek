f7c1b07ba7e9f9a4342627b2057d38db45dcf76a
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-08-06 13:09:30 -0700

    chore(Vuex): Clean up Vuex and push missing adds.

--- templates/src/js/store/index.js
+++ templates/src/js/store/index.js
@@ -4,8 +4,10 @@ import Vuex from 'vuex';
 // Vuex modules:
 import example from './modules/example';
 
+Vue.use(Vuex);
+
 export default new Vuex.Store({
-    modules: {
-        example
-    }
+  modules: {
+    example,
+  },
 });
